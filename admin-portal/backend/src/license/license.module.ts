56177fac2528db06099779311593967c2900ec71
darkgrecher <hashanrajakaruna123456@gmail.com>
2026-03-03 14:17:48 +0530

    remove frontend and backend

--- admin-portal/backend/src/license/license.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LicenseController } from './license.controller';
-import { LicenseService } from './license.service';
-
-@Module({
-  controllers: [LicenseController],
-  providers: [LicenseService],
-})
-export class LicenseModule {}
